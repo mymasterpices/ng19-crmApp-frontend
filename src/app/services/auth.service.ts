b763c4fcadbf73bd1ac8d1ee222ae20341601926
mymasterpices <sooraj.sharma@rkjewellers.in>
2025-06-23 12:36:56 +0530

    angular enviroment setup

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Location } from '@angular/common';
 
